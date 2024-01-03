8eaec17507217d89b054b25aa78e0eb5a6d19e2a
chrisbarba123 <chris.barba@bjss.com>
2024-01-03 13:46:46 +0000

    create service, entity / database classes

---
+++ src/main/java/com/dev/books/services/BookService.java
@@ -0,0 +1,5 @@
+package com.dev.books.services;
+
+public interface BookService {
+
+}
---
+++ src/main/java/com/dev/books/services/impl/BookServiceImpl.java
@@ -0,0 +1,11 @@
+package com.dev.books.services.impl;
+
+import com.dev.books.services.BookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+
+
+}
